efbcf7261d800367d931869c4ee32a6ed1296764
Jlvsl <lvsl@gisuni.com>
2021-02-26 10:45:42 +0800

    1

--- express-api/config/mysqlConfig.js
+++ express-api/config/mysqlConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
     connection: {
-        host: "8go.win",
-        user: "chatbot",
-        password: "chatbot",
-        database: "chatbot",
+        host: "数据库host",
+        user: "用户名",
+        password: "密码",
+        database: "数据库名",
         useConnectionPooling: true,
         port: 3306
     }
